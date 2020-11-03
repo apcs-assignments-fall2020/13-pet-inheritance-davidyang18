e7d8716321ed5e0fc98160f02f0ecf4c9faaf257
davidyang18 <2817822828@qq.com>
2020-11-03 05:29:22 +0000

    Added Dog inheritance

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,37 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
 
+    public Dog() {
+        super("Max", 2);
+        this.breed = "Alaskan Malamute";
+    }
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Bark!");
+    }
 
     // toString method
-
+    public String toString() {
+        String str = "Breed:" + breed;
+        return str;
+    }
 
     // Getter
-
+    public String getBreed() {
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed) {
+        if (breed.trim().length() != 0) {
+            this.breed = breed;
+        }
+    }
 }
